acc411d5e9ce02f784082b0e03d542601a883168
gastonmsuarez <gaston.ms90@gmail.com>
2023-02-27 23:05:52 -0300

    entrega 1

---
+++ src/Components/CardWidget/Cardwidget.jsx
@@ -0,0 +1,13 @@
+import {BsFillCartCheckFill } from "react-icons/bs"
+
+const Cardwidget = () => {
+  return (
+    <div>
+
+        < BsFillCartCheckFill />
+
+    </div>
+  )
+}
+
+export default Cardwidget
---
+++ src/Components/ItemListConteiner/ItemListConteiner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ItemListConteiner = (greeting) => {
+  return (
+    <div>
+      <h4>Bienvenidos</h4>
+    </div>
+  );
+};
+
+export default ItemListConteiner;
